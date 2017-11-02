87d80ccca6d62f2079d9bc037aa8bacd74461495
songxiaoyum <33242008+songxiaoyum@users.noreply.github.com>
2017-11-02 09:44:01 +0800

    Add files via upload

---
+++ sf.py
@@ -0,0 +1,23 @@
+import smtplib
+import email.mime.multipart
+import email.mime.text
+
+msg = email.mime.multipart.MIMEMultipart()
+msg['from'] = '13218852211@163.com'
+msg['to'] = '2584465565@qq.com'
+msg['subject'] = 'test'
+content = '''''
+    你好，
+            这是一封自动发送的邮件。
+
+
+'''
+txt = email.mime.text.MIMEText(content)
+msg.attach(txt)
+
+smtp = smtplib
+smtp = smtplib.SMTP()
+smtp.connect('smtp.163.com', '25')
+smtp.login('13218852211@163.com', '607412mei')
+smtp.sendmail('13218852211@163.com', '2584465565@qq.com', str(msg))
+smtp.quit()
